ba8d14a9bd595f6d6cf844c1c82728d2d9a0795c
Aaron Martens <95230814+LegacyOfTheArdor@users.noreply.github.com>
2025-11-03 14:14:24 +0100

    good ish car movement
    with gravity

--- Scripts/Car_controller.gd
+++ Scripts/Car_controller.gd
@@ -1,56 +1,77 @@
-extends Node3D
+extends CharacterBody3D
 
-#gets all the car parts
-@onready var Front_Wheel_Left= $Body/Front_Wheel_Left
-@onready var Front_Wheel_Right= $Body/Front_Wheel_Right
-@onready var Back_Wheel_Left= $Body/Back_Wheel_Left
-@onready var Back_Wheel_Right= $Body/Back_Wheel_Right
 
-#sets the max for speed en turn circle
-@export var Max_Turn = 0.9
-@export var Max_Speed = 10
+@onready var Front_Wheel_Left  = $Body/Front_Wheel_Left
+@onready var Front_Wheel_Right = $Body/Front_Wheel_Right
+@onready var Back_Wheel_Left   = $Body/Back_Wheel_Left
+@onready var Back_Wheel_Right  = $Body/Back_Wheel_Right
+
+
+@export var Max_Turn_Angle = 0.5      
+@export var Max_Speed      = 20.0      
+@export var Acceleration   = 10.0      
+@export var Brake_Strength = 15.0     
+@export var Turn_Speed     = 1.5       
+@export var Gravity        = 30.0      
+
 
 var Left_Input: float = 0.0
 var Right_Input: float = 0.0
 var Brake_Input: float = 0.0
 var Acceleration_Input: float = 0.0
 
-var Speed_Input: float = 0.0
+
+var Speed: float = 0.0
+var Direction: float = 1.0
 var Steer_Input: float = 0.0
+var Steer_Angle: float = 0.0
+var Forward_Direction: Vector3
 
-var Forward_Direction
-var velocity = Vector3.ZERO
 
-func set_inputs(left: float, right: float, brake:float, acceleration: float):
+func set_inputs(left: float, right: float, brake: float, acceleration: float):
 	Left_Input = left
 	Right_Input = right
 	Brake_Input = brake
 	Acceleration_Input = acceleration
-	
-	
-	
-func _physics_process(delta: float):
-	
-	
-	# abs for absulote and we need some speed to turn the car
-	if abs(Speed_Input) > 0.1:
-		rotation.y += Steer_Input * Max_Turn
-		
-	Forward_Direction = -transform.basis.z
-	
-	 # Set velocity (normalize for constant speed)
-	velocity = Forward_Direction * Speed_Input
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * Max_Speed
 
-	global_position += velocity * delta
+
+func _physics_process(delta: float):
+	#gravity
+	if not is_on_floor():
+		velocity.y -= Gravity * delta
+	else:
+		velocity.y = 0.0
+
+	#steering
+	Steer_Input = Left_Input - Right_Input
+	var target_steer = Steer_Input * Max_Turn_Angle
+	Steer_Angle = lerp(Steer_Angle, target_steer, 0.2)
+
+	#forward and backwards and come to a standstill if both are pressed
+	if Acceleration_Input > 0.1 and Brake_Input < 0.1:
+		Direction = 1
+		Speed += Acceleration * delta
+	elif Brake_Input > 0.1 and Acceleration_Input < 0.1:
+		Direction = -1
+		Speed += Acceleration * delta
+	else:
+		Speed = lerp(Speed, 0.0, 0.05)
+
+	# Clamp to max speed
+	Speed = clamp(Speed, 0.0, Max_Speed)
+
 	
-func _process(delta):
+	if abs(Speed) > 0.1:
+		rotation.y += Steer_Angle * Turn_Speed * delta * Direction
+
 	
+	Forward_Direction = -transform.basis.z
+	velocity.x = Forward_Direction.x * Speed * Direction
+	velocity.z = Forward_Direction.z * Speed * Direction
+
 	
-	Speed_Input = Acceleration_Input - Brake_Input * Max_Speed
-	Steer_Input = Left_Input - Right_Input * Max_Turn
-	Front_Wheel_Left.rotation.x = Steer_Input
-	Front_Wheel_Right.rotation.x = Steer_Input
+	move_and_slide()
+
 	
-	
+	Front_Wheel_Left.rotation.y = Steer_Angle
+	Front_Wheel_Right.rotation.y = Steer_Angle
